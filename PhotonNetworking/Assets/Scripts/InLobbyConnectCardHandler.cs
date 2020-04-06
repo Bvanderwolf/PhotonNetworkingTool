8683f5c8bc457912103222738af83788009d14f3
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-04-06 13:07:48 +0200

    Added in lobby content handler for handling animation events

--- PhotonNetworking/Assets/Scripts/InLobbyConnectCardHandler.cs
+++ PhotonNetworking/Assets/Scripts/InLobbyConnectCardHandler.cs
@@ -17,6 +17,13 @@ public class InLobbyConnectCardHandler : ConnectCardAbstract
     [SerializeField]
     private GameObject m_Content;
 
+    private Animator m_ContentAnimator;
+    private InLobbyContentHandler m_ContentHandler;
+
+    private enum ContentType { None, RoomList, CreateRoom }
+    private ContentType m_DetourContent;
+    private ContentType m_ContentOpen;
+
     public override void Init()
     {
         base.Init();
@@ -24,19 +31,98 @@ public class InLobbyConnectCardHandler : ConnectCardAbstract
         m_JoinRoomButton.onClick.AddListener(OnJoinRoomButtonClick);
         m_CreateRoomButton.onClick.AddListener(OnCreateRoomButtonClick);
         m_LeaveLobbyButton.onClick.AddListener(OnLeaveLobbyButtonClick);
+
+        m_ContentAnimator = m_Content.GetComponent<Animator>();
+        m_ContentHandler = m_Content.GetComponent<InLobbyContentHandler>();
+
+        m_ContentHandler.ContentOpened += OnContentOpened;
+        m_ContentHandler.ContentClosed += OnContentClosed;
     }
 
     private void OnJoinRoomButtonClick()
     {
-        if (!m_Content.activeInHierarchy)
+        switch (m_ContentOpen)
         {
-            m_Content.SetActive(true);
+            case ContentType.None:
+                OpenContent(ContentType.RoomList);
+                break;
+            case ContentType.RoomList:
+                CloseContent();
+                break;
+            case ContentType.CreateRoom:
+                CloseContent(ContentType.RoomList);
+                break;
         }
+
+        m_JoinRoomButton.interactable = false;
+    }
+
+    private void OpenContent(ContentType content)
+    {
+        if(content != ContentType.None)
+        {
+            m_ContentAnimator.SetTrigger("Open");
+            m_ContentOpen = content;
+        }     
+    }
+
+    private void OnContentOpened()
+    {
+        switch (m_ContentOpen)
+        {
+            case ContentType.RoomList:
+                m_JoinRoomButton.interactable = true;
+                break;
+            case ContentType.CreateRoom:
+                m_CreateRoomButton.interactable = true;
+                break;
+        }
+    }
+
+    private void OnContentClosed()
+    {
+        if(m_DetourContent == ContentType.None)
+        {
+            switch (m_ContentOpen)
+            {
+                case ContentType.RoomList:
+                    m_JoinRoomButton.interactable = true;                  
+                    break;
+                case ContentType.CreateRoom:
+                    m_CreateRoomButton.interactable = true;
+                    break;
+            }
+            m_ContentOpen = ContentType.None;
+        }
+        else
+        {
+            OpenContent(m_DetourContent);
+            m_DetourContent = ContentType.None;
+        }
+    }
+
+    private void CloseContent(ContentType detourContent = ContentType.None)
+    {
+        m_ContentAnimator.SetTrigger("Close");
+        m_DetourContent = detourContent;
     }
 
     private void OnCreateRoomButtonClick()
     {
-        
+        switch (m_ContentOpen)
+        {
+            case ContentType.None:
+                OpenContent(ContentType.CreateRoom);
+                break;
+            case ContentType.RoomList:
+                CloseContent(ContentType.CreateRoom);
+                break;
+            case ContentType.CreateRoom:
+                CloseContent();
+                break;
+        }
+
+        m_CreateRoomButton.interactable = false;
     }
 
     private void OnLeaveLobbyButtonClick()
---
+++ PhotonNetworking/Assets/Scripts/InLobbyContentHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class InLobbyContentHandler : MonoBehaviour
+{
+    public event Action ContentOpened;
+    public event Action ContentClosed;  
+
+    public void OnContentOpened()
+    {
+        ContentOpened?.Invoke();
+    }
+
+    public void OnContentClosed()
+    {
+        ContentClosed?.Invoke();
+    }
+}
