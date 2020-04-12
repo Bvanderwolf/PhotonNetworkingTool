d477bae6faf984a92eed2e496f61755ef7877a77
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-04-12 15:22:01 +0200

    Added Chat messaging and scrolling to chatcontent

--- PhotonNetworking/Assets/Scripts/ConnectCards/InRoomContentHandler.cs
+++ PhotonNetworking/Assets/Scripts/ConnectCards/InRoomContentHandler.cs
@@ -4,8 +4,10 @@
     using Photon.Realtime;
     using Singletons;
     using System;
+    using System.Collections;
     using System.Linq;
     using UnityEngine;
+    using UnityEngine.EventSystems;
     using UnityEngine.UI;
     using Utils;
 
@@ -48,15 +50,28 @@
         }
 
         //*should implement for each chatbot message enum a message
-        private Func<string, string>[] m_ChatBotMessages;
+        private Func<string, string>[] m_ChatBotMessages = new Func<string, string>[]
+        {
+            (name) => name + " left the room",
+            (name) => name + " joined the room",
+            (name) => name + " is the new host"
+        };
 
         private PlayerItem[] m_PlayerItems;
+
+        private PhotonView m_PV;
+
+        private const int CHAT_LINE_AMOUNT = 45;
+        private const int CHAT_INPUT_CHAR_AMOUNT = 50;
+        private int m_CurrentChatLine = 0;
 
         public override void Init()
         {
             base.Init();
 
             m_PlayerItems = new PlayerItem[InRoomManager.MAX_PLAYERS_AMMOUNT];
+            m_PV = GetComponent<PhotonView>();
+            m_ChatInput.characterLimit = CHAT_INPUT_CHAR_AMOUNT;
 
             for (int i = 0; i < m_PlayerItems.Length; i++)
             {
@@ -145,24 +160,103 @@
 
         public void SetActiveStateOfChatContent(bool value)
         {
+            m_ChatContent.gameObject.SetActive(value);
+        }
+
+        public void HandlePlayerChatInput(string input)
+        {
+            ClearChatInput();
+            ResetFocusOnChatInput();
+
+            if (IsClearInput(input))
+            {
+                ClearChat();
+                return;
+            }
+
+            m_PV.RPC("UpdateChatWithMessage", RpcTarget.AllViaServer, input, PhotonNetwork.LocalPlayer);
+        }
+
+        private void ResetFocusOnChatInput()
+        {
+            var eventSystem = EventSystem.current;
+            if (eventSystem.currentSelectedGameObject == m_ChatInput.gameObject)
+                m_ChatInput.OnPointerClick(new PointerEventData(eventSystem));
+        }
+
+        private void ClearChatInput()
+        {
+            m_ChatInput.text = "";
+        }
+
+        private void ClearChat()
+        {
+            m_ChatText.text = "";
+            m_CurrentChatLine = 0;
+        }
+
+        private void TrimFirstLineOfChat()
+        {
+            var text = m_ChatText.text;
+            //grab index of first newline from where i can start the substring
+            var indexOfNewLine = text.IndexOf("\n") + 1;
+            m_ChatText.text = text.Substring(indexOfNewLine);
+        }
+
+        private void AddTextToChat(string text, Player sender)
+        {
+            if (m_CurrentChatLine != CHAT_LINE_AMOUNT)
+                m_CurrentChatLine++;
+            else
+                TrimFirstLineOfChat();
+
+            var time = string.Format("({0:t}) ", DateTime.Now);
+            var user = string.Format("({0})", sender.NickName);
+            m_ChatText.text += (time + user + ": " + text + "\n");
+        }
+
+        private void AddTextToChat(ChatBotMessages message, string nameOfPlayer)
+        {
+            if (m_CurrentChatLine != CHAT_LINE_AMOUNT)
+                m_CurrentChatLine++;
+            else
+                TrimFirstLineOfChat();
+
+            var time = string.Format("({0:t})", DateTime.UtcNow);
+            var text = m_ChatBotMessages[(int)message];
+            m_ChatText.text += (time + ": " + text(nameOfPlayer) + "\n");
+        }
+
+        private bool IsClearInput(string input)
+        {
+            return input == "/Clear" || input == "/clear";
         }
 
         public void OnMasterClientChange(Player newMaster)
         {
             SetActiveStateOfPlayerListContent(false);
             SetActiveStateOfPlayerListContent(true);
+            AddTextToChat(ChatBotMessages.NewMasterClient, newMaster.NickName);
         }
 
         public void OnPlayerEnteredRoom(Player player)
         {
             SetActiveStateOfPlayerListContent(false);
             SetActiveStateOfPlayerListContent(true);
+            AddTextToChat(ChatBotMessages.PlayerJoined, player.NickName);
         }
 
         public void OnPlayerLeftRoom(Player player)
         {
             SetActiveStateOfPlayerListContent(false);
             SetActiveStateOfPlayerListContent(true);
+            AddTextToChat(ChatBotMessages.PlayerLeft, player.NickName);
+        }
+
+        [PunRPC]
+        private void UpdateChatWithMessage(string message, Player sender)
+        {
+            AddTextToChat(message, sender);
         }
     }
 }
